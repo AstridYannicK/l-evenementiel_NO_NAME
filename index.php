ad45ecfce8b9831f4b91fb84fa7363eeeb7a7b0d
AstridYannicK <100100049+AstridYannicK@users.noreply.github.com>
2022-02-21 00:35:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>L'évènementiel No Name</title>
+    <link rel="icon" href="images/fav.jpg" sizes="32x32" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header>
+
+    <a href="#" class="logo">L'évènementiel NO NAME</a>
+
+    <div id="menu-bar" class="fas fa-bars"></div>
+
+    <nav class="navbar">
+        <a href="#home">Accueil</a>
+        <a href="#Services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#Staff">Staff</a>
+        <a href="#contact">contact</a>
+        <a href="#blog">blog</a>
+    </nav>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <span>Agence d'organisation d'evenements</span>
+        <h3>Nous rendons chaque moment special!</h3>
+        <a href="#" class="btn">Voir +</a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<section class="step-container">
+
+    <div class="step">
+        <span>01</span>
+        <h3>engagement</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum porro iure esse laudantium aspernatur, veniam sunt provident eius cum ab.</p>
+        <a href="#" class="btn">Voir +</a>
+    </div>
+
+    <div class="step">
+        <span>02</span>
+        <h3>save the date</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum porro iure esse laudantium aspernatur, veniam sunt provident eius cum ab.</p>
+        <a href="#" class="btn">Voir +</a>
+    </div>
+
+    <div class="step">
+        <span>03</span>
+        <h3>wedding ceremony</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum porro iure esse laudantium aspernatur, veniam sunt provident eius cum ab.</p>
+        <a href="#" class="btn">Voir +</a>
+    </div>
+
+</section>
+
+<!-- vendor section starts  -->
+
+<section class="vendor" id="vendor">
+
+    <div class="heading">
+        <h1>Nos Services</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/img1.jpg" alt="">
+            <div class="info">
+                <h3>Tenues de mariées</h3>
+                <p>Nous vous porposons des robes de mariees, des modèles pour les filles et garçons d'honneur.</p>
+                <a href="#" class="btn">Voir +</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img2.jpg" alt="">
+            <div class="info">
+                <h3>Fleuriste</h3>
+                <p>Nous disposons d'une equipe de décorateurs extremement qualifiés.</p>
+                <a href="#" class="btn">Voir +</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img3.jpg" alt="">
+            <div class="info">
+                <h3>Gateaux de mariage</h3>
+                <p>Nous proposons des gateries qui conviennent aux besoins du client.</p>
+                <a href="#" class="btn">Voir +</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img4.jpg" alt="">
+            <div class="info">
+                <h3>Salles de ceremonies</h3>
+                <p>Nous disposons d'un eventail de salles qui conviennent a tout type de client. </p>
+                <a href="#" class="btn">Voir +</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img5.jpg" alt="">
+            <div class="info">
+                <h3>Sonorisation et Lumineux</h3>
+                <p>Nous disposons des appareils de dernieres generation pour l'animation de vos évènements. </p>
+                <a href="#" class="btn">Voir +</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/img6.jpg" alt="">
+            <div class="info">
+                <h3>Photographe</h3>
+                <p>Nous avons a notre disposition les meilleur en questionde prise d'images></p>
+                <a href="#" class="btn">Voir +</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- vendor section ends -->
+
+<!-- client section starts  -->
+
+<section class="clients" id="clients">
+
+    <div class="heading">
+        <h1>Nos précedents clients</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <div class="row">
+
+        <div class="box-container">
+            <div class="box">
+                <img src="images/2.jpg" alt="">
+                <div class="info">
+                    <h3>01. Joceline & Alex</h3>
+                    <p> <i class="fas fa-quote-left"></i> Une tres belle ceremonie qui s'est deroulee le Samedie 18 Decembre 2021 a Yaounde! <i class="fas fa-quote-right"></i> </p>
+                </div>
+            </div>
+            <div class="box">
+                <img src="images/1.jpg" alt="">
+                <div class="info">
+                    <h3>02. Madeleine & Alex</h3>
+                    <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
+                </div>
+            </div>
+        </div>
+
+        <img src="images/divider.png" class="divider" alt="">
+
+        <div class="box-container">
+            <div class="box">
+                <div class="info">
+                    <h3>03. Clyde & Arsene</h3>
+                    <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
+                </div>
+                <img src="images/client-3.jpg" alt="">
+            </div>
+            <div class="box">
+                <div class="info">
+                    <h3>04. mark & alice</h3>
+                    <p> <i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet illo aut repudiandae nihil quidem perspiciatis natus placeat, inventore, vitae, quia totam ad dolores velit. Cumque, alias ullam. Nihil, aperiam hic! <i class="fas fa-quote-right"></i> </p>
+                </div>
+                <img src="images/client-4.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- client section ends -->
+
+<!-- team section starts  -->
+
+<section class="team" id="team">
+
+    <div class="heading">
+        <h1>Notre Staff</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/3.png" alt="">
+            <h3>Merime NKEMBE</h3>
+            <span>PDG</span>
+            <p>Specialiste en communication evenementielle!</p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/team-2.png" alt="">
+            <h3>john deo</h3>
+            <span>wedding planner</span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta nisi ea quisquam ipsa molestias itaque, quod quis id doloremque similique!</p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/astrid.jpg" alt="">
+            <h3>Astrid Yannick MAKO</h3>
+            <span>Web designer</span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta nisi ea quisquam ipsa molestias itaque, quod quis id doloremque similique!</p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- team section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <div class="heading">
+        <h1>Contactez-nous</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <form action="">
+        <div class="inputBox">
+            <input type="text" placeholder="Votre nom">
+            <input type="email" placeholder="Votre email">
+        </div>
+        <div class="inputBox">
+            <input type="number" placeholder="Votre numéro de téléphone">
+            <input type="text" placeholder="Votre adresse">
+        </div>
+        <textarea placeholder="Veuillez ecrire votre message ici" name="" id="" cols="30" rows="10"></textarea>
+        <input type="submit" value="Envoyer" class="btn">
+    </form>
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- blog section starts  -->
+
+<section class="blog" id="blog">
+
+    <div class="heading">
+        <h1>our latest blogs</h1>
+        <img src="images/header-img.png" alt="">
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/blog-1.jpg" alt="">
+            <div class="info">
+                <h3> <span>01. </span> blog title goes here </h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium odio a voluptate. Eaque vel ab dolorem, earum optio aliquam.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/blog-2.jpg" alt="">
+            <div class="info">
+                <h3> <span>02. </span> blog title goes here </h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium odio a voluptate. Eaque vel ab dolorem, earum optio aliquam.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/blog-3.jpg" alt="">
+            <div class="info">
+                <h3> <span>03. </span> blog title goes here </h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium odio a voluptate. Eaque vel ab dolorem, earum optio aliquam.</p>
+                <a href="#" class="btn">learn more</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blog section ends-->
+
+<!-- footer section  -->
+
+<div class="footer"> &copy; <span>L'évènementiel No Name</span> | Tous droits reservés </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script> 
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+}
+
+</script>
+
+
+</body>
+</html>
